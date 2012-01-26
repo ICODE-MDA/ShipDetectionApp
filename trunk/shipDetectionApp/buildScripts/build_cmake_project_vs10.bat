3419f8e63f7c1034c78fe6997191b1925ce88520
bryan@midnightsignal.com <bryan@midnightsignal.com@fdd52888-2fd2-40d9-2659-c92be34f9d9a>
2012-01-26 20:21:50 +0000

    modified the build scripts to agree with getting started documentation.
    modified code to agree with 3rd-party folders on icode-mda site

--- trunk/shipDetectionApp/buildScripts/build_cmake_project_vs10.bat
+++ trunk/shipDetectionApp/buildScripts/build_cmake_project_vs10.bat
@@ -2,11 +2,29 @@
 
 set OLDPATH=%PATH%
 
-call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
-set PATH="C:\Program Files\CMake 2.8\bin";%PATH%
+set PATH="C:\Program Files (x86)\CMake 2.8\bin";%PATH%
 
-call cmake_arguments.bat
+set ossim_trunk_dir="D:/libraries/ossim/ossim_trunk"
+set ossim_third_party_dir="D:/libraries/ossim/3rd-party"
+set ossim_lib_dir="D:/libraries/ossim/ossim_build/Release"
+
+set ossim_third_party_lib=%ossim_third_party_dir%/lib/win32
+set ossim_include_dir=%ossim_trunk_dir%/ossim/include
+
+cmake -G "Visual Studio 10"^
+ -DCMAKE_BUILD_TYPE=Release^
+ -DCMAKE_INCLUDE_PATH=%ossim_third_party_dir%/include^
+ -DOSSIM_INCLUDE_DIR=%ossim_include_dir%^
+ -DOPENCV_INCLUDE_DIR=%ossim_third_party_dir%/include^
+ -DSHAPELIB_INCLUDE_DIR=%ossim_third_party_dir%/include^
+ -DCVBLOBS_INCLUDE_DIR=%ossim_third_party_dir%/include^
+ -DOPENCV_LIBRARIES=%ossim_third_party_lib%/cv210.lib;%ossim_third_party_lib%/cvaux210.lib;%ossim_third_party_lib%/cxcore210.lib;%ossim_third_party_lib%/cxts210.lib;%ossim_third_party_lib%/ml210.lib;%ossim_third_party_lib%/highgui210.lib^
+ -DOSSIM_LIBRARIES=%ossim_lib_dir%/ossim.lib^
+ -DSHAPELIB_LIRARY=%ossim_third_party_lib%/shapelib.lib^
+ -DCVBLOBS_LIRARY=%ossim_third_party_lib%/cvblobslib.lib^
+ ../
 
 set PATH=%OLDPATH%
 set OLDPATH=
