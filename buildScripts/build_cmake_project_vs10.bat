9ef812667b9451255ca16213a61d18d4b927b2c7
bryan@midnightsignal.com <bbagnall@users.noreply.github.com>
2012-01-26 20:37:43 +0000

    changed build script so it will make a batch file to run the app in release mode with the dlls in the path.

--- buildScripts/build_cmake_project_vs10.bat
+++ buildScripts/build_cmake_project_vs10.bat
@@ -6,9 +6,9 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
 set PATH="C:\Program Files (x86)\CMake 2.8\bin";%PATH%
 
-set ossim_trunk_dir="D:/libraries/ossim/ossim_trunk"
-set ossim_third_party_dir="D:/libraries/ossim/3rd-party"
-set ossim_lib_dir="D:/libraries/ossim/ossim_build/Release"
+set ossim_trunk_dir=D:/libraries/ossim/ossim_trunk
+set ossim_third_party_dir=D:/libraries/ossim/3rd-party
+set ossim_lib_dir=D:/libraries/ossim/ossim_build/Release
 
 set ossim_third_party_lib=%ossim_third_party_dir%/lib/win32
 set ossim_include_dir=%ossim_trunk_dir%/ossim/include
@@ -26,5 +26,15 @@ cmake -G "Visual Studio 10"^
  -DCVBLOBS_LIRARY=%ossim_third_party_lib%/cvblobslib.lib^
  ../
 
+rem Write out bat file to set the path when running
+mkdir Release
+echo @echo off >> .\Release\shipDetectionApp.bat
+echo set OLDPATH=%%PATH%% >> .\Release\shipDetectionApp.bat
+echo set PATH=%ossim_third_party_dir%\bin\win32;%ossim_lib_dir%;%%PATH%% >> .\Release\shipDetectionApp.bat
+echo shipDetectionApp.exe %%1 %%2 >> .\Release\shipDetectionApp.bat
+echo set PATH=%%OLDPATH%% >> .\Release\shipDetectionApp.bat
+echo set OLDPATH= >> .\Release\shipDetectionApp.bat
+
+ 
 set PATH=%OLDPATH%
 set OLDPATH=
